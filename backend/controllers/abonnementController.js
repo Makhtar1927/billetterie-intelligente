d9784542924355de31cdf07bbdf722675baaeb8d
Papem Makhtar Aidara <papemakhtaraidara@gmail.com>
2026-09-18 16:21:21 +0000

    Erreurs add

--- backend/controllers/abonnementController.js
+++ backend/controllers/abonnementController.js
@@ -5,7 +5,7 @@ const SUBSCRIPTION_SERVICE_URL = process.env.SUBSCRIPTION_SERVICE_URL || 'https:
 
 const handleError = (err, res) => {
   if (err.response) {
-    return res.status(err.response.status).json(err.response.data);
+    return res.status(err.response.status).json(err.response.);
   }
   res.status(500).json({ success: false, message: err.message });
 };
